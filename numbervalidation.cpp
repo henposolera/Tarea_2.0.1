898988ae027fee2cbdfcf5a3a1f22c82f39be4cc
henposolera <31905743+henposolera@users.noreply.github.com>
2017-10-02 20:22:32 -0600

    Create numbervalidation.cpp

---
+++ numbervalidation.cpp
@@ -0,0 +1,13 @@
+int validInput()
+{
+    int x;
+    std::cin >> x;
+    while(std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        std::cout << "Bad entry.  Enter a NUMBER: ";
+        std::cin >> x;
+    }
+    return x;
+}
